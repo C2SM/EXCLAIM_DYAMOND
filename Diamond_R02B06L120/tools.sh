44f40160db92b2d4f6317a7555747412f9ad5cd7
Matthieu <matthieu.leclair@c2sm.ethz.ch>
2025-05-01 12:35:57 +0200

    add: R02B06

---
+++ Diamond_R02B06L120/tools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/
+
+check_available(){
+   echo -n "Check for ${1} ... "
+    if [ -r "${2}" ]; then
+        echo "${2} AVAILABLE"
+    else
+        echo "${2} MISSING"
+        exit 1
+    fi
+}
