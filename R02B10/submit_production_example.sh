d8e6eb72a0a0fb335316e8187a827ac7ab940575
Matthieu <matthieu.leclair@c2sm.ethz.ch>
2025-05-15 17:15:23 +0200

    use Praveen's defaults

--- R02B10/submit_production_example.sh
+++ R02B10/submit_production_example.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/bash
 
-export ACCOUNT="cwp03"
-export UENV="icon-wcp/v1:rc4"
-export VIEW="icon"
-
 # First time
 export restart_interval="P11D"
 export end_date="2020-02-01T00:00:00"
 
 # Subsequent runs
 export lrestart="true"
-export restart_interval="P1M"
 export end_date="2021-04-01T00:00:00"
 
 ./DYAMOND_R02B10L120.run
