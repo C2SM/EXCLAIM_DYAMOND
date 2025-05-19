7905da278c3f970ac11e37bd29b3eb1d3ab80fcc
pothapakula <praveen.pothapakula@c2sm.ethz.ch>
2025-05-19 11:37:29 +0200

    Modified Precpi and other variables acumulation window

--- R02B10/namelists.sh
+++ R02B10/namelists.sh
@@ -87,10 +87,10 @@ main_atmo_nml(){
  restart_write_mode           = "joint procs multifile"  ! For Large Runs Joint procs is recomemded from our experience
  lflux_avg                    = .true.   ! "FALSE" output fluxes are accumulated from the beginning of the run, "TRUE" average values
  lnetcdf_flt64_output         = .false.  ! Default value is false (CK)
- precip_interval              = "P1D"    ! NEW ! Works The precipitation value is accumulated in these interval otherwise accumulated fromm begining of the run
- runoff_interval              = "P1D"    ! NEW ! Works The runoff is accumalted in this inetrval else accumulated from bengining.
- maxt_interval                = "P1D"    ! NEW ! Works Interval at which Max/Min 2m temperture are calculated
- melt_interval                = "P1D"    ! NEW ! Works CLM community has this , Can not find discription
+ precip_interval              = "PT15M"    ! NEW ! Works The precipitation value is accumulated in these interval otherwise accumulated fromm begining of the run
+ runoff_interval              = "PT3H"    ! NEW ! Works The runoff is accumalted in this inetrval else accumulated from bengining.
+ maxt_interval                = "PT3H"    ! NEW ! Works Interval at which Max/Min 2m temperture are calculated
+ melt_interval                = "PT3H"    ! NEW ! Works CLM community has this , Can not find discription
  lmask_boundary               = .true.   ! NEW ! Works if interpolation zone should be masked in triangular output.
  dt_hailcast                  = 900.
 /
