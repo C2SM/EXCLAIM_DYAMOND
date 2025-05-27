2bd97a48abacc91a67e06db904187d02dd299ad5
pothapakula <praveen.pothapakula@c2sm.ethz.ch>
2025-05-27 23:04:49 +0200

    changes for outdir

--- R02B10/lfs_Permissions_folder.sh
+++ R02B10/lfs_Permissions_folder.sh
@@ -4,6 +4,7 @@ EXPDIR="${SCRATCH}/DYAMOND_experiments/${EXPNAME}_main"
 
 export EXPDIR="${SCRATCH}/DYAMOND_experiments/${EXPNAME}_main"
 for ((k=1; k<=15; k++)); do
+    outdir="${EXPDIR}/out_${k}"
     mkdir -p "${outdir}"
     lfs setstripe -i 0 -c 20 ${outdir}
 done
